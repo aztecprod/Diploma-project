768302bdd710c460c2cceb6dd8e2ed7174a479b9
aztecprod <08sasha@list.ru>
2023-09-25 17:41:06 +0300

    Update backup.tf

--- terraform/backup.tf
+++ terraform/backup.tf
@@ -12,9 +12,8 @@ resource "yandex_compute_snapshot_schedule" "default" {
   }
 
   disk_ids = [yandex_compute_instance.web1.boot_disk[0].disk_id, 
-              yandex_compute_instance.web2.boot_disk[0].disk_id, 
-              yandex_compute_instance.prometheus.boot_disk[0].disk_id, 
-              yandex_compute_instance.grafana.boot_disk[0].disk_id, 
+              yandex_compute_instance.web2.boot_disk[0].disk_id,  
+              yandex_compute_instance.zabbix.boot_disk[0].disk_id, 
               yandex_compute_instance.elasticsearch.boot_disk[0].disk_id, 
               yandex_compute_instance.kibana.boot_disk[0].disk_id, 
               yandex_compute_instance.sshgw.boot_disk[0].disk_id]
