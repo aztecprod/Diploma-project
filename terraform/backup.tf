dd4b37e4ddf5e763dcd972e7c55c450a15d2550e
aztecprod <08sasha@list.ru>
2023-09-30 23:54:17 +0300

    Update backup.tf

--- terraform/backup.tf
+++ terraform/backup.tf
@@ -2,10 +2,10 @@ resource "yandex_compute_snapshot_schedule" "default" {
   name = "default"
 
   schedule_policy {
-    expression = "0 0 * * SUN"
+    expression = "0 5 ? * *"
   }
 
-  snapshot_count = 1
+  snapshot_count = 7
 
   snapshot_spec {
     description = "weekly"
